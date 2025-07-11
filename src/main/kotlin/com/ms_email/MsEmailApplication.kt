88bd7a2a604b7dda1f329beeda39e04a363121e8
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-11 13:14:18 -0300

    Initial commit

---
+++ src/main/kotlin/com/ms_email/MsEmailApplication.kt
@@ -0,0 +1,11 @@
+package com.ms_email
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsEmailApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsEmailApplication>(*args)
+}
---
+++ src/test/kotlin/com/ms_email/MsEmailApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ms_email
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsEmailApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
